741cb23da9dc159b0277510abbc05d485886ffd0
farazjahangir <jahangirfaraz98@gmail.com>
2018-10-10 12:26:46 -0700

    skill progress section and some work on responsive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import html from './images/skills/html.png'
 import css from './images/skills/css.png'
+import js from './images/skills/js.png'
+import reactLogo from './images/skills/reactLogo.png'
+
 
 class App extends Component {
   render() {
@@ -22,27 +25,51 @@ class App extends Component {
         <div className="progress-section my-6">
           <h1 className="text-center">My Skills</h1>
           <div className="container-fluid">
-            <div className="row my-5">
+
+            <div className="row my-3">
               <div className="col-6 max-width">
-                <img src={html} width={"200px"} />
+                <img src={html} width={"110px"} />
               </div>
-              <div className="col-6 my-6">
-                <div class="progress">
-                  <div class="progress-bar" style={{ width: "80%" }}>80%</div>
+              <div className="col-6 my-5">
+                <div className="progress">
+                  <div className="progress-bar progress-bar-striped" style={{ width: "80%" }}>80%</div>
                 </div>
               </div>
             </div>
 
-            <div className="row my-6">
-              <div className="col-6 max-width">
-                <img src={css} width={"170px"} />
+            <div className="row my-2 content-align-right">
+              <div className="col-6 my-5 content-align-right">
+                <div className="progress">
+                  <div className="progress-bar progress-bar-striped" style={{ width: "60%" }}>60%</div>
+                </div>
               </div>
-              <div className="col-6 my-6">
-                <div class="progress">
-                  <div class="progress-bar" style={{ width: "60%" }}>60%</div>
+              <div className="col-6 max-width">
+                <img src={css} width={"80px"} />
+              </div>
+            </div>
+
+            <div className="row my-2">
+              <div className="col-6 max-width">
+                <img src={js} width={"110px"} />
+              </div>
+              <div className="col-6 my-5">
+                <div className="progress">
+                  <div className="progress-bar progress-bar-striped" style={{ width: "80%" }}>50%</div>
                 </div>
               </div>
             </div>
+
+            <div className="row my-2 content-align-right">
+              <div className="col-6 my-5 content-align-right">
+                <div className="progress">
+                  <div className="progress-bar progress-bar-striped" style={{ width: "60%" }}>50%</div>
+                </div>
+              </div>
+              <div className="col-6 max-width">
+                <img src={reactLogo} width={"120px"} />
+              </div>
+            </div>
+
 
           </div>
         </div>
